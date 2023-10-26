9504ed4099dcdf19292c0b0033d502f01d147dfc
agent19music <seanmotanya@gmail.com>
2023-10-26 13:43:08 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,20 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {
+      projects.map((project)=>(
+        <div key={project.id}>
+          <p>{project.name}</p>
+          <p>{project.about}</p>
+          <ProjectItem 
+          key={project.id}
+          technologies={project.technologies}/>
+        </div>
+
+      ))
+      }
+      </div>
     </div>
   );
 }
